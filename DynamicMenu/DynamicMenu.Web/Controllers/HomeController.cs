625aac16981b0aa090e9a953fd8cdbe8bad4bde3
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-05-21 19:54:16 +0200

    HomeController.cs: inject Data Context, added Menu action

--- DynamicMenu/DynamicMenu.Web/Controllers/HomeController.cs
+++ DynamicMenu/DynamicMenu.Web/Controllers/HomeController.cs
@@ -1,29 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DynamicMenu.Web.Controllers
 {
+    using DataLayer;
+    using ViewModels;
+
     public class HomeController : Controller
     {
+        public DataContext DataContext { get; }
+
+        public HomeController(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
         public IActionResult Index()
         {
+            ViewData["Title"] = "Domovska stranka";
             return View();
+        }
+
+        public IActionResult Menu()
+        {
+            using (var bc = new BussinesContext(DataContext))
+            {
+                var categories = bc.GetCategories(bc.GetMenus());
+                var viewModel = new MenusViewModel { Categories = categories.Where(c=>c.Menu.MenuHierarchyLevel==MenuHierarchyLevel.Root).ToList()};
+                return PartialView("_CategoryMenu", viewModel);
+            }
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Title"] = "O nas";
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
+            ViewData["Title"] = "Kontakt";
             return View();
         }
 
