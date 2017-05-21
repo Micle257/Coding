d038a44d6a9a81b2f40a8ca7cd3a85716510f536
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-05-21 19:50:10 +0200

    added MenusViewModel

---
+++ DynamicMenu/DynamicMenu.Web/ViewModels/MenusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicMenu.Web.ViewModels
+{
+    using DataLayer;
+
+    public class MenusViewModel
+    {
+        public IList<Category> Categories { get; set; }
+    }
+}
