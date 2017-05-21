b5e1f8eb7960527c5b47493fbfe7d68bc1ac4d33
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-05-21 19:48:33 +0200

    added extensions methods for working with SP in EFC

---
+++ DynamicMenu/DynamicMenu.DataLayer/Extensions/DataReaderExtensions.cs
@@ -0,0 +1,49 @@
+namespace DynamicMenu.DataLayer.Extensions {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Reflection;
+
+    /// <summary>
+    /// Contains extension methods for <see cref="IDataReader" />.
+    /// </summary>
+    public static class DataReaderExtensions
+    {
+        internal static IList<T> MapModel<T>(this IDataReader reader)
+            where T : new()
+        {
+            var result = new List<T>();
+            var properties = reader.GetColumns<T>();
+            while (reader.Read())
+            {
+                var row = new T();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    var name = reader.GetName(i);
+                    if (properties.TryGetValue(name, out PropertyInfo property))
+                    {
+                        var value = reader.GetValue(i);
+                        property.SetValue(row, value == DBNull.Value ? null : value);
+                    }
+                }
+                result.Add(row);
+            }
+            return result;
+        }
+
+        static IDictionary<string, PropertyInfo> GetColumns<T>(this IDataReader reader)
+        {
+            var result = new Dictionary<string,PropertyInfo>();
+            var type = typeof(T);
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                var name = reader.GetName(i);
+                var fixedName = name.Replace("-", string.Empty).Replace("_", string.Empty);
+                var property = type.GetProperty(fixedName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (property != null)
+                    result[name] = property;
+            }
+            return result;
+        }
+    }
+}
---
+++ DynamicMenu/DynamicMenu.DataLayer/Extensions/DbContextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicMenu.DataLayer.Extensions
+{
+    using System.Data;
+    using System.Data.Common;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Contains extension methods for <see cref="DbContext" />.
+    /// </summary>
+    public static class DbContextExtensions
+    {
+        public static IList<T> ExecuteStoredProcedure<T>(this DbContext context, string spName)
+            where T : new()
+        {
+            using (var reader = context.InvokesStoredProcedure(spName).ExecuteReader())
+            {
+                return reader.MapModel<T>();
+            }
+        }
+
+       static DbCommand InvokesStoredProcedure(this DbContext context, string spName)
+       {
+           var cmd = context.Database.GetDbConnection().CreateCommand();
+           cmd.CommandText = spName;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            context
+                .Database.OpenConnection();
+           return cmd;
+       }
+    }
+}
