68b4d565f793ad4a8632e76c519a997a3bc366b5
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-05-21 19:48:55 +0200

    added Category model

---
+++ DynamicMenu/DynamicMenu.DataLayer/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicMenu.DataLayer
+{
+    /// <summary>
+    /// Represents a data model for category.
+    /// </summary>
+    public class Category
+ {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <value>
+        /// The <see cref="String"/>.
+        /// </value>
+        public string Title => Menu.DisplayName;
+        /// <summary>
+        /// Gets or sets the reference to menu entity.
+        /// </summary>
+        /// <value>
+        /// The <see cref="Menu"/>.
+        /// </value>
+        public Menu Menu { get; set; }
+        /// <summary>
+        /// Gets or sets the children categories.
+        /// </summary>
+        /// <value>
+        /// The list of the <see cref="Category"/>.
+        /// </value>
+        public IList<Category> Children { get; set; }
+    }
+}
