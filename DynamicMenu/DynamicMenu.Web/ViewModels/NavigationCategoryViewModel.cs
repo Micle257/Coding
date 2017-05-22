d6cc591b36f5ba20ea2bb1e385691312e3c18278
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-05-22 18:07:31 +0200

    <Web> added NavigationCategoryViewModel.cs

---
+++ DynamicMenu/DynamicMenu.Web/ViewModels/NavigationCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicMenu.Web.ViewModels
+{
+    /// <summary>
+    /// Represents a view model for category navigation in partial view.
+    /// </summary>
+    public class NavigationCategoryViewModel
+    {
+    }
+}
